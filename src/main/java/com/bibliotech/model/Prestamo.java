3149f9867f903d5d1843df97ebf0ae2fee287c38
josiasvilches <jl.vilches@alumno.um.edu.ar>
2026-04-27 14:29:31 -0300

    refactor(model): actualizar Prestamo incorporando fechas de devolucion y logica inmutable

--- src/main/java/com/bibliotech/model/Prestamo.java
+++ src/main/java/com/bibliotech/model/Prestamo.java
@@ -3,10 +3,22 @@ package main.java.com.bibliotech.model;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record Prestamo(String id, String socioDni, String recursoIsbn, LocalDate fechaPrestamo, boolean devuelto) {
-
-    // Constructor auxiliar para crear préstamos nuevos fácilmente
+public record Prestamo(
+        String id,
+        String socioDni,
+        String recursoIsbn,
+        LocalDate fechaPrestamo,
+        LocalDate fechaDevolucionEsperada,
+        LocalDate fechaDevolucionReal,
+        boolean devuelto
+) {
+    // Constructor auxiliar para préstamos nuevos (Asignamos 14 días de plazo estándar)
     public Prestamo(String socioDni, String recursoIsbn) {
-        this(UUID.randomUUID().toString(), socioDni, recursoIsbn, LocalDate.now(), false);
+        this(UUID.randomUUID().toString(), socioDni, recursoIsbn, LocalDate.now(), LocalDate.now().plusDays(14), null, false);
+    }
+
+    // Método para mantener la inmutabilidad: retorna un nuevo record con el estado final
+    public Prestamo registrarDevolucion(LocalDate fechaReal) {
+        return new Prestamo(id, socioDni, recursoIsbn, fechaPrestamo, fechaDevolucionEsperada, fechaReal, true);
     }
 }
